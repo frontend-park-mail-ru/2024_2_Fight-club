50d9d0945a108748a5663f9d711a43549f550fe3
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-11-14 23:07:39 +0300

    feat: add image scrolling

--- components/AdPage/AdPage.ts
+++ components/AdPage/AdPage.ts
@@ -4,13 +4,15 @@ import { AdvertData, ProfileInfo } from '../../modules/Types';
 import { calculateAge } from '../../modules/Utils';
 import ReactiveComponent from '../ReactiveComponent/ReactiveComponent';
 
-const MAIN_IMG_DIV_SELECTOR = '.js-main-img-div';
 const SECONDARY_IMG_SELECTOR = '.js-carousel-img';
 const FULLSCREEN_OVERLAY_SELECTOR = '.js-fullscreen-overlay';
 const FULLSCREEN_OVERLAY_HIDDEN_CLASSNAME =
     'ad-page__fullscreen-overlay_hidden';
 
 export default class AdPage extends ReactiveComponent {
+    #scrollWidth: number = 0;
+    #scrollWasAutomated: boolean = false;
+
     constructor(
         parent: HTMLElement,
         data: AdvertData,
@@ -45,24 +47,74 @@ export default class AdPage extends ReactiveComponent {
         ) as HTMLDivElement;
 
         const mainImgDiv = this.thisElement.querySelector(
-            MAIN_IMG_DIV_SELECTOR
+            '.js-main-img-div'
         ) as HTMLDivElement;
 
         overlay.onclick = mainImgDiv.onclick = () =>
             overlay.classList.toggle(FULLSCREEN_OVERLAY_HIDDEN_CLASSNAME);
 
+        const carousel = this.thisElement.querySelector(
+            '.ad-page__gallery__carousel'
+        ) as HTMLDivElement;
+        this.#scrollWidth = carousel.clientWidth;
+        carousel.onscroll = () => {
+            if (this.#scrollWasAutomated) return;
+
+            const newIndex = Math.ceil(carousel.scrollLeft / this.#scrollWidth);
+            console.log(carousel.scrollLeft, this.#scrollWidth);
+
+            if (this.state.currentIndex !== newIndex) {
+                this.state.currentIndex = newIndex;
+            }
+        };
+        carousel.onscrollend = () => {
+            this.#scrollWasAutomated = false;
+        };
+
+        (
+            this.thisElement.querySelector(
+                '.js-prev-image-button'
+            ) as HTMLButtonElement
+        ).onclick = (e) => {
+            e.stopPropagation();
+            carousel.scrollLeft -= this.#scrollWidth;
+        };
+        (
+            this.thisElement.querySelector(
+                '.js-next-image-button'
+            ) as HTMLButtonElement
+        ).onclick = (e) => {
+            e.stopPropagation();
+            carousel.scrollLeft += this.#scrollWidth;
+        };
+
         carouselImages.forEach((el: HTMLImageElement, index) => {
-            el.onclick = () => this.showImage(carouselImages, index);
+            el.onclick = () => {
+                this.showImage(carousel, carouselImages, index);
+            };
         });
 
-        this.showImage(carouselImages, this.state.currentIndex as number);
+        this.showImage(
+            carousel,
+            carouselImages,
+            this.state.currentIndex as number
+        );
     }
 
-    showImage(carouselImages: NodeListOf<HTMLImageElement>, index: number) {
-        if (index !== this.state.currentIndex) this.state.currentIndex = index;
-
+    showImage(
+        carousel: HTMLDivElement,
+        carouselImages: NodeListOf<HTMLImageElement>,
+        index: number
+    ) {
         carouselImages[index].classList.add(
-            'advert-images-carousel__secondary_img--current'
+            'ad-page__gallery__secondary_img--current'
         );
+
+        if (index === this.state.currentIndex) return;
+        const delta = index - (this.state.currentIndex as number);
+
+        this.state.currentIndex = index;
+        carousel.scrollLeft += delta * this.#scrollWidth;
+        this.#scrollWasAutomated = true;
     }
 }
