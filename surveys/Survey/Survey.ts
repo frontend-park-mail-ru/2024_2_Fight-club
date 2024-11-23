b7930683150c90d924c176386b98c0513c27fb10
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-11-23 12:58:05 +0300

    feat: add question switching

--- surveys/Survey/Survey.ts
+++ surveys/Survey/Survey.ts
@@ -1,15 +1,33 @@
 'use strict';
 
+const QUESTIONS = [
+    {
+        id: 0,
+        title: 'Как вам ваша жизнь?',
+    },
+    {
+        id: 1,
+        title: 'Как вам наш сервис?',
+    },
+];
+
 /** Карточка объявления на главной странице */
 export default class Survey {
     #template;
     #parent;
-    #data;
+    #questions;
+    #answers;
+    #currentIndex;
+
+    #surveyTitleElement: HTMLParagraphElement;
+    #nextButtonElement: HTMLButtonElement;
 
     constructor(parent: HTMLElement, data) {
-        this.#data = data;
+        this.#questions = QUESTIONS;
         this.#parent = parent;
         this.#template = Handlebars.templates['Survey.hbs'];
+        this.#answers = new Map<number, number>();
+        this.#currentIndex = 0;
     }
 
     addEventListeners() {
@@ -20,16 +38,42 @@ export default class Survey {
         xBtn.onclick = () => {
             console.log('I was clicked');
         };
+
+        this.#nextButtonElement.onclick = () => this.displayNextQuestion();
     }
 
     render() {
         this.#parent.insertAdjacentHTML(
             'beforeend',
-            this.#template(this.#data)
+            this.#template({
+                title: this.#questions[0].title,
+            })
         );
 
         requestAnimationFrame(() => {
+            this.#surveyTitleElement = this.#parent.querySelector(
+                '.survey__title'
+            ) as HTMLParagraphElement;
+
+            this.#nextButtonElement = this.#parent.querySelector(
+                '.survey__next-button'
+            ) as HTMLButtonElement;
             this.addEventListeners();
         });
+    }
+
+    displayNextQuestion() {
+        this.#currentIndex++;
+
+        if (this.#currentIndex >= this.#questions.length) {
+            return;
+        }
+
+        this.#surveyTitleElement.textContent =
+            this.#questions[this.#currentIndex].title;
+
+        if (this.#currentIndex === this.#questions.length - 1) {
+            this.#nextButtonElement.textContent = 'Завершить';
+        }
     }
 }
