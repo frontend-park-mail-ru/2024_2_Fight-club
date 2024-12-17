9c7c9cd4796497594ae771b303a6f84abdf12467
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-12-17 13:18:33 +0300

    build: switch sass api to modern

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import handlebarsCompilePlugin from './src/vite-plugins/hbs-recompile.js';
 /** @type {import('vite').UserConfig} */
 export default defineConfig({
     plugins: [handlebarsCompilePlugin()],
+    css: {
+        preprocessorOptions: {
+            sass: {
+                api: 'modern',
+            },
+        },
+    },
     server: {
         host: '0.0.0.0',
         port: 5173,
