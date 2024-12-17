af2b02cb5800724095bca116de9749568f24dd5a
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-12-17 23:35:22 +0300

    fix(chat): send message on pure enter press

--- src/components/ChatWindow/ChatWindow.ts
+++ src/components/ChatWindow/ChatWindow.ts
@@ -108,7 +108,7 @@ export default class ChatWindow extends BaseComponent {
         };
 
         textArea.onkeydown = (event) => {
-            if (event.ctrlKey && event.key === 'Enter') {
+            if (!event.shiftKey && !event.ctrlKey && event.key === 'Enter') {
                 const text = textArea.value;
                 this.sendMessage(text);
                 textArea.value = '';
