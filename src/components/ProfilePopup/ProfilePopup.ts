dbc05840619a421aacc3b7200576b943b7855bf4
rasulov1337 <117843890+rasulov1337@users.noreply.github.com>
2024-12-18 18:44:21 +0300

    fix: deremove messenger from ProfilePopup

--- src/components/ProfilePopup/ProfilePopup.ts
+++ src/components/ProfilePopup/ProfilePopup.ts
@@ -35,10 +35,6 @@ class ProfilePopup {
                 title: 'Мои объявления',
                 href: '/ads/?author=me',
             },
-            chats: {
-                title: 'Мессенджер',
-                href: '/chats',
-            },
             logout: {
                 title: 'Выйти',
                 href: '',
